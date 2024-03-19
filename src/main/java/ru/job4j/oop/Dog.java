df11dfc13060fca24ab4ce29341493335e8fddcf
Vasilieva <masha.maria.vas@bk.ru>
2024-03-19 21:26:17 +0400

    1.1. Создание объекта [#165452]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychk = new Dog();
+    }
+}
+
+
+
+
