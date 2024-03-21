024246e9dcf2b2bca55a86fde393b6d3a7f6ee50
Vasilieva <masha.maria.vas@bk.ru>
2024-03-21 22:25:46 +0400

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 5, "I am first message");
+        Error error3 = new Error(false, 15, "I am second message");
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+    }
+}
