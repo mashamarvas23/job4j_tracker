20c3195526c1c69806f2385732a44f2db155f675
Vasilieva <masha.maria.vas@bk.ru>
2024-04-15 08:59:34 +0400

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,15 +14,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findByName(String key) {
@@ -39,5 +32,27 @@ public class Tracker {
 
     public Item[] findAll() {
         return Arrays.copyOf(items, size);
+    }
+
+    private int indexOf(int id) {
+        int result = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                result = index;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index != -1) {
+            items[index] = item;
+            items[index].setId(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
