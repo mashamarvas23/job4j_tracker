37b0b63394d65f2ab21c703db13a4143c5441fa1
Vasilieva <masha.maria.vas@bk.ru>
2024-04-15 19:37:10 +0400

    5. Tracker - хранилище [#396 #491230]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -48,12 +48,11 @@ public class Tracker {
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         if (index != -1) {
+            item.setId(id);
             items[index] = item;
-            items[index].setId(id);
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     public void delete(int id) {
