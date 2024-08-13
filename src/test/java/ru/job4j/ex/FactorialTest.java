3ace4eb4aa5f48acd79f1789b02e7c6fa04fb916
Vasilieva <masha.maria.vas@bk.ru>
2024-08-13 09:34:22 +0400

    0.3.Тестирование исключений с  junit [#219366]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -16,4 +16,12 @@ public class FactorialTest {
         );
         assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
     }
+
+    @Test
+    public void whenSuccess() {
+        Factorial factorial = new Factorial();
+        int excpectedResult = 120;
+        int actualResult = factorial.calc(5);
+        assertThat(actualResult).isEqualTo(excpectedResult);
+    }
 }
