165cab2ff0478b9a18cab0accc201877e09f0954
Vasilieva <masha.maria.vas@bk.ru>
2024-08-19 17:08:32 +0400

    9. Изменить программу Tracker из 2-го модуля [#10039]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -33,7 +33,7 @@ public class TrackerTest {
         Item second = new Item("Second");
         tracker.add(first);
         tracker.add(second);
-        List<Item> result =tracker.findAll();
+        List<Item> result = tracker.findAll();
         assertThat(result.size()).isEqualTo(2);
     }
 
