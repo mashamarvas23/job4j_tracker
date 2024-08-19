bd2f494f59f22851b3d896628d29bd2d685699f6
Vasilieva <masha.maria.vas@bk.ru>
2024-08-19 15:23:42 +0400

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.search;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenFindByAddress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("nsk");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenFindByPhoneNumber() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Maria", "Vasileva", "534999", "Samara")
+        );
+        ArrayList<Person> persons = phones.find("534");
+        assertThat(persons.size()).isEqualTo(2);
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+        assertThat(persons.get(1).getSurname()).isEqualTo("Vasileva");
+    }
+
+    @Test
+    public void whenNotFoundBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Maria", "Vasileva", "534999", "Samara")
+        );
+        ArrayList<Person> persons = phones.find("Petrov");
+        assertThat(persons.size()).isEqualTo(0);
+    }
+}
