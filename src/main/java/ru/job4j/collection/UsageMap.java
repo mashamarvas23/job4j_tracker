afc296ba3b86ced8883fbfc7d0de702330c3bb93
Vasilieva <masha.maria.vas@bk.ru>
2024-08-27 21:15:12 +0400

    0. Map, HashMap [#212652 #510728]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("masha.maria.vas@bk.ru", "Maria Vasileva");
+        map.put("stas_mikhailov@bk.ru", "Stas Mikhailov");
+        map.put("dominik_joker@bk.ru", "Dominik Joker");
+        map.put("timati@bk.ru", "Timati");
+        map.put("timati@bk.ru", "Timati Ivanov");
         for (String key : map.keySet()) {
             System.out.println(map.get(key));
         }
