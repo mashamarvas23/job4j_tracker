636ee40ee0c542dea2b8723caf84da77574d7e4d
Vasilieva <masha.maria.vas@bk.ru>
2024-08-27 21:19:34 +0400

    5. Очередь с приоритетом на LinkedList [#41670 #509768]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -7,16 +7,13 @@ public class PriorityQueue {
 
     public void put(Task task) {
         int index = 0;
-        if (tasks.isEmpty()) {
-            tasks.add(task);
-        }
         for (Task element : tasks) {
             if (task.getPriority() < element.getPriority()) {
-                this.tasks.add(index, task);
                 break;
             }
             index++;
         }
+        this.tasks.add(index, task);
     }
 
     public Task take() {
